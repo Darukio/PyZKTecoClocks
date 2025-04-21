e4b60d39c430f31644cef3c76ec16b9e3dc4bad8
Darukio <damian-200@live.com.ar>
2025-04-21 09:22:48 -0300

    style(ui)[base_dialog]: added documentation for methods, including args, errors, and attributes

--- scripts/ui/base_dialog.py
+++ scripts/ui/base_dialog.py
@@ -26,6 +26,15 @@ import os
 
 class BaseDialog(QDialog):
     def __init__(self, parent=None, window_title=""):
+        """
+        Initializes the base dialog window with the specified parent and window title.
+        Args:
+            parent (QWidget, optional): The parent widget for the dialog. Defaults to None.
+            window_title (str, optional): The title of the dialog window. Defaults to an empty string.
+        Raises:
+            BaseError: If an exception occurs during initialization, it raises a BaseError with code 3501
+                       and the exception message.
+        """
         try:
             super().__init__(parent)
             self.setWindowTitle(window_title)
@@ -48,6 +57,16 @@ class BaseDialog(QDialog):
         pass
 
     def adjust_size_to_table(self):
+        """
+        Adjusts the size of the dialog window to fit the content of the table widget.
+        This method resizes the columns of the table widget to fit their contents and calculates
+        the required width and height of the table based on its content. It ensures that the
+        dialog window does not exceed the available screen height, applying a margin if necessary.
+        Finally, it resizes the dialog window to accommodate the adjusted table dimensions.
+        Notes:
+            - An extra width adjustment is added to account for margins and the button bar.
+            - The height is capped to fit within the available screen height minus a margin.
+        """
         # Adjust columns based on the content
         self.table_widget.resizeColumnsToContents()
         
@@ -63,6 +82,15 @@ class BaseDialog(QDialog):
         self.resize(table_width + 120, table_height)  # Extra adjustment for margins and button bar
     
     def center_window(self):
+        """
+        Centers the window on the current screen.
+
+        This method calculates the available geometry of the current screen
+        and moves the window to the center of the screen.
+
+        Returns:
+            None
+        """
         screen = self.screen()  # Get the current screen
         screen_rect = screen.availableGeometry()  # Get screen available geometry
         self.move(screen_rect.center() - self.rect().center())  # Move to center
