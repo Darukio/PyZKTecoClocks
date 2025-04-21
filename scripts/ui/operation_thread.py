f3fca493bdf142cb6327c59b38217ae642249726
Darukio <damian-200@live.com.ar>
2025-04-21 09:42:45 -0300

    feat(ui)[operation_thread]: added type annotations docs(ui)[operation_thread]: added or updated method docs to explain functionality del(ui)[operation_thread]: commented out start_time log refactor(ui)[operation_thread]: renamed variables to reflect their purpose

--- scripts/ui/operation_thread.py
+++ scripts/ui/operation_thread.py
@@ -17,34 +17,75 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from typing import Callable
 from PyQt5.QtCore import QThread, pyqtSignal
 from scripts.common.utils.errors import BaseError
 
 class OperationThread(QThread):
-    op_updated = pyqtSignal(dict)
-    op_terminated = pyqtSignal(float)
+    op_terminate = pyqtSignal(dict)
+    op_start_time = pyqtSignal(float)
     progress_updated = pyqtSignal(int, str, int, int)  # Signal for progress
 
-    def __init__(self, op_func, selected_devices=None, parent=None):
+    def __init__(self, op_func: Callable, selected_ips: list[str] = None, parent = None):
+        """
+        Initializes the OperationThread instance.
+
+        Args:
+            op_func (Callable): The operation function to be executed in the thread.
+            selected_ips (list[str], optional): A list of selected IP addresses. Defaults to None.
+            parent (QObject, optional): The parent object for the thread. Defaults to None.
+        """
         super().__init__(parent)
-        self.op_func = op_func
-        self.selected_devices = selected_devices
+        self.op_func: Callable = op_func
+        self.selected_ips: list[str] = selected_ips
+        self.result: dict = {}
 
     def run(self):
+        """
+        Executes the operation function (`op_func`) with the selected IPs or without them,
+        handles the result, and emits appropriate signals.
+
+        This method is designed to run in a separate thread to perform operations
+        asynchronously. It captures any exceptions raised during execution and
+        raises a `BaseError` with a specific error code and message.
+
+        Attributes:
+            selected_ips (list): A list of selected IP addresses to pass to the operation
+                function. If not provided, the operation function is called without IPs.
+            result (dict): The result of the operation function execution.
+
+        Emits:
+            op_terminate (dict): Signal emitted with the result of the operation function
+                or an empty dictionary if the result is `None`.
+
+        Raises:
+            BaseError: If an exception occurs during the execution of the operation
+                function, a `BaseError` is raised with error code 3000 and the exception
+                message.
+        """
         try:
-            import time
-            start_time = time.time()
-            if self.selected_devices:
-                self.result = self.op_func(self.selected_devices, emit_progress=self.emit_progress)
+            #import time
+            #start_time: float = time.time()
+            if self.selected_ips:
+                self.result: dict = self.op_func(self.selected_ips, emit_progress=self.emit_progress)
             else:
-                self.result = self.op_func(emit_progress=self.emit_progress)
+                self.result: dict = self.op_func(emit_progress=self.emit_progress)
             if self.result is None:
-                self.op_updated.emit({})
+                self.op_terminate.emit({})
             else:
-                self.op_updated.emit(self.result)
-            self.op_terminated.emit(start_time)
+                self.op_terminate.emit(self.result)
+            #self.op_start_time.emit(start_time)
         except Exception as e:
             raise BaseError(3000, str(e), "critical")
 
-    def emit_progress(self, percent_progress=None, device_progress=None, processed_devices=None, total_devices=None):
+    def emit_progress(self, percent_progress: int = None, device_progress: str = None, processed_devices: int = None, total_devices: int = None):
+        """
+        Emits a progress update signal with the provided progress details.
+
+        Args:
+            percent_progress (int, optional): The overall percentage of progress completed. Defaults to None.
+            device_progress (str, optional): A string describing the progress of the current device. Defaults to None.
+            processed_devices (int, optional): The number of devices that have been processed so far. Defaults to None.
+            total_devices (int, optional): The total number of devices to be processed. Defaults to None.
+        """
         self.progress_updated.emit(percent_progress, device_progress, processed_devices, total_devices)  # Emit the progress signal
